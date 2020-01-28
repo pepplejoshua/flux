9f87e5069153ac1e9a498e12427bc5bc22c5ed07
Pepple, Joshua <pepple.joshua@uleth.ca>
2020-01-28 22:34:16 +0000

    Bool fix

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import pytest
 def pytest_addoption(parser):
     parser.addoption('--exp', default='0', help='Pass your expected result')
     parser.addoption('--ln', default='0', help='Pass an expression to evaluate')
+    parser.addoption('--etype', default='int', help='Data type expected back')
 
 @pytest.fixture
 def exp(request):
@@ -11,3 +12,7 @@ def exp(request):
 @pytest.fixture
 def ln(request):
     return request.config.getoption('--ln')
+
+@pytest.fixture
+def etype(request):
+    return request.config.getoption('--etype')
--- test_flux.py
+++ test_flux.py
@@ -7,8 +7,11 @@ def eval(eq):
         res = entry(True, False, test=True, code=eq)
         return res
     
-def test_eval(ln, exp):
-        req = exp
+def test_eval(ln, exp, etype):
+        if etype == 'int':
+                req = int(exp)
+        elif etype == 'bool':
+                req = bool(exp)
         res = eval(ln)
         cprint(f"\n\nCalculation: {ln} ?= {exp}", 'green')
         cprint(f"Result[{res}] == Expected[{req}]", 'yellow')
